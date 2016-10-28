292afb8bea8e8faa78f7016fef8cecd209f3551e
Ryan Allen <Ryan Allen>
2016-10-28 01:04:30 -0400

    Moved everything to Analyzer project

---
+++ MineSweeper.Analyzer/Solvers/BaseImplementationSolver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MineSweeper.Logic;
+using MineSweeper.Models;
+
+namespace MineSweeper.Solvers
+{
+    public class BaseImplementationSolver : ISolver
+    {
+        public Move GetNextMove(Cell[,] grid)
+        {
+	        for (var x = 0; x < grid.GetLength(1); x++)
+	        {
+		        for (var y = 0; y < grid.GetLength(0); y++)
+		        {
+			        if (grid[y, x].State == CellState.Revealed)
+			        {
+				        var value = grid[y, x].Value;
+				        var neighbors = this.GetAdjacentCells(grid, x, y);
+				        var possibleMineCount = 0;
+						var flaggedMineCount = 0;
+
+				        foreach (var neighbor in neighbors)
+				        {
+					        if (neighbor.State == CellState.Hidden)
+					        {
+						        possibleMineCount++;
+					        }
+							else if (neighbor.State == CellState.Flagged)
+							{
+								flaggedMineCount++;
+								possibleMineCount++;
+							}
+				        }
+
+				        if (possibleMineCount == value)
+				        {
+					        foreach (var neighbor in neighbors)
+					        {
+						        if (neighbor.State == CellState.Hidden)
+						        {
+							        return new Move {MoveType = MoveType.Flag, X = neighbor.X, Y = neighbor.Y};
+						        }
+					        }
+				        }
+
+						if (flaggedMineCount == value && possibleMineCount > flaggedMineCount)
+						{
+							foreach (var neighbor in neighbors)
+							{
+								if (neighbor.State == CellState.Hidden)
+								{
+									return new Move { MoveType = MoveType.Click, X = neighbor.X, Y = neighbor.Y };
+								}
+							}
+						}
+					}
+		        }
+			}
+
+	        for (var x = 0; x < grid.GetLength(1); x++)
+	        {
+		        for (var y = 0; y < grid.GetLength(0); y++)
+		        {
+			        var cell = grid[y, x];
+			        if (cell.State == CellState.Hidden)
+			        {
+						return new Move { MoveType = MoveType.Click, X = cell.X, Y = cell.Y };
+					}
+		        }
+	        }
+	        return null;
+        }
+
+        private List<Cell> GetAdjacentCells(Cell[,] grid, int x, int y)
+        {
+            var xOptions = new[] { x - 1, x, x + 1 }.Where(v => v >= 0 && v < grid.GetLength(1));
+            var yOptions = new[] { y - 1, y, y + 1 }.Where(v => v >= 0 && v < grid.GetLength(0));
+            var allPairs = xOptions.SelectMany(xValue => yOptions.Select(yValue => new { xValue, yValue })).Where(s => s.xValue != x || s.yValue != y);
+            return allPairs.Select(p => grid[p.yValue, p.xValue]).ToList();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ MineSweeper.Solver.Python/BaseImplementationSolver.py
@@ -0,0 +1,73 @@
+#region ---- Models ----
+
+class Cell(object):
+	def __init__(self, x, y, value, state):
+		self.X = x
+		self.Y = y
+		self.Value = value
+		self.State = state
+
+class CellState:
+	HIDDEN = 0
+	FLAGGED = 1
+	REVEALED = 2
+
+class Move(object):
+	def __init__(self, x, y, moveType):
+		self.X = x
+		self.Y = y
+		self.MoveType = moveType
+
+class MoveType:
+	CLICK = 0
+	FLAG = 1
+
+#endregion
+
+def getNeighbors(x, y, grid):
+	for nx in xrange(max(0, x - 1), min(x + 2, len(grid))):
+		for ny in xrange(max(0, y - 1), min(y + 2, len(grid[0]))):
+			if nx != 0 or ny != 0:
+				yield grid[ny][nx]
+                              
+import random
+ 
+# TODO: implement Solver
+class Solver(object):
+	# returns a Move given a grid, which is a 2D array of Cell objects
+	def GetNextMove(self, grid):
+		n = len(grid)
+
+		for x in range(n):
+			for y in range(n):
+				if grid[y][x].State == CellState.REVEALED:
+					value = grid[y][x].Value
+					neighbors = list(getNeighbors(x, y, grid))
+					possible_mine_count = 0
+					flagged_mine_count = 0
+
+					for neighbor in neighbors:
+						if neighbor.State == CellState.HIDDEN:
+							possible_mine_count += 1
+						elif neighbor.State == CellState.FLAGGED:
+							possible_mine_count += 1
+							flagged_mine_count += 1
+
+					if possible_mine_count == value:
+						for neighbor in neighbors:
+							if neighbor.State == CellState.HIDDEN:
+								return Move(neighbor.X, neighbor.Y, MoveType.FLAG)
+
+					if flagged_mine_count == value and possible_mine_count > flagged_mine_count:
+						for neighbor in neighbors:
+							if neighbor.State == CellState.HIDDEN:
+								return Move(neighbor.X, neighbor.Y, MoveType.CLICK)
+
+		for x in range(n):
+			for y in range(n):
+				cell = grid[y][x]
+				if cell.State == CellState.HIDDEN:
+					return Move(cell.X, cell.Y, MoveType.CLICK)
+		# poss = [(x, y) for x in range(len(grid)) for y in range(len(grid[0])) if grid[y][x].State == CellState.HIDDEN]
+		# random.shuffle(poss)
+		# return Move(poss[0][0], poss[0][1], MoveType.CLICK)
